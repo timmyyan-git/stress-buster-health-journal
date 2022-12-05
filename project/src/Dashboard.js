ca0a421758caffce4896013fe862d2dafb0cd093
Daniel Clements <drclements@liberty.edu>
2022-12-05 11:18:15 -0500

    health cards & basic styling

--- project/src/Dashboard.js
+++ project/src/Dashboard.js
@@ -4,8 +4,46 @@ function Dashboard (){
 
     return (
         <div>
-            <em>Dashboard</em>
-            <HealthCard />
+            <h3>Your <em>Stress Busters</em> Dashboard</h3>
+            <p>Daily average of stress buster habits</p>
+            <section className="health-snap">
+                <section className="health-card">
+                    <b>Nature</b>
+                    <div>Time in Nature: {0} mins</div>
+                </section>
+                <section className="health-card">
+                    <b>Physical Activity</b>
+                    <div>Time Spent: {0} mins</div>
+                    <div>Average Steps: {0}</div>  
+                </section>
+                <section className="health-card">
+                    <b>Relationships</b>
+                    <div>Quality Time Spent: {0}</div>
+                    <div>Positive Social Connections: {0}</div>
+                </section>
+                <section className="health-card">
+                    <b>Mindfulness</b>
+                    <div>Time Spent: {0}mins</div>
+                    <div>Mindful Breaks Taken: {0}</div>
+                </section>
+                <section className="health-card">
+                    <b>Balanced Nutrition </b>
+                    <div>Water Consumption: {0} L </div>
+                    <div>Balanced Meal Rating: {0} / 10 </div>
+                </section>
+                <section className="health-card ">
+                    <b>Sleep</b>
+                    <div>Hours Slept: {0}</div>
+                    <div> Quality of Sleep: {0} / 10</div>
+                </section>
+                <section className="health-card">
+                    <b>Mental Healthcare</b>
+                    <div>Media Consumption: {0} mins</div>
+                    <div>Journal Completion: {0}</div>
+                </section>
+            </section>
+
+            {/* <HealthCard /> */}
         </div>
     )
 }
