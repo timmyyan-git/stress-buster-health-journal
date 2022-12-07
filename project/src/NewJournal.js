35b7a5ee587a4a70231a9d970449a56c7ea28356
Ana Rosario <arosario.mrd@gmail.com>
2022-12-06 22:23:29 -0500

    newjournal

--- project/src/NewJournal.js
+++ project/src/NewJournal.js
@@ -1,11 +1,11 @@
 import JournalForm from "./JournalForm"
-import RandomQuote from "./RandomQuote"
-import MainPage from "./MainPage"
+import moment from "moment";
 
 function NewJournal ({onRandomQuote}){
     const {author, content} = onRandomQuote
     return (
         <div>
+            
            <h2> New Journal Entry</h2> 
            <h4>reflecting on your day can help release pent up tension. </h4>
            <h4> write freely or use our daily quote as a prompt! </h4>
