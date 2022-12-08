abaffdb11e954244c119eafe2eeaa713a9d7b773
Timmy Yan <timmyyan2000@gmail.com>
2022-12-08 00:18:12 -0500

    forum added comments

--- project/src/CommentForm.js
+++ project/src/CommentForm.js
@@ -1,11 +1,37 @@
-function CommentForm (){
+import { useState } from "react";
+
+const styles = {
+    container: {
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+    },
+    textBox: {
+        marginBottom: "20px",
+    },
+    submit: {
+        height: "50px",
+        width: "200px",
+    },
+};
+
+function CommentForm({ addComment }) {
+    const [comment, setComment] = useState("");
 
     return (
-        <div>
-            <em>Comment</em>
-            
+        <div style={styles.container}>
+            <textarea
+                style={styles.textBox}
+                rows="4"
+                cols="100"
+                placeholder="Enter comment here..."
+                onChange={(e) => setComment(e.target.value)}
+            ></textarea>
+            <button style={styles.submit} onClick={() => addComment(comment)}>
+                Submit
+            </button>
         </div>
-    )
+    );
 }
 
-export default CommentForm
+export default CommentForm;
